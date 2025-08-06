4dc72f055948910b9524c88736444d713470004d
Nagarjun A <nagarjun.avala.official@gmail.com>
2025-08-06 15:59:41 +0530

    Updated binaryTargets = ["native", "rhel-openssl-3.0.x"] in scehama

--- lib/db.ts
+++ lib/db.ts
@@ -1,5 +1,6 @@
 // lib/db.ts or src/db.ts
-import { PrismaClient } from "@/prisma/prismaClient"
+// import { PrismaClient } from "@/prisma/prismaClient"
+import { PrismaClient } from '@prisma/client';
 
 const globalForPrisma = globalThis as unknown as {
     prisma: PrismaClient | undefined
